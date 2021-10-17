3aabbf70a0638aa1014cb8f8fd741bb8de319e44
Ibrahim193084 <ibrahimibr193084@gmail.com>
2021-10-18 01:31:36 +0600

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div>
+      <Home></Home>
    
     </div>
   );
